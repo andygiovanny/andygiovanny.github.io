b9da3442b52a9dda8574e8734e6d6d869e6b3511
andygiovanny <30598728+andygiovanny@users.noreply.github.com>
2017-09-21 16:47:01 +0700

    Test

--- js/pomodoro-timer.js
+++ js/pomodoro-timer.js
@@ -2,28 +2,28 @@ var $tomato = $('#tomato');
 var x = setInterval(dance,1000);
 
 function dance(){
-    if ($tomato.attr('src') == ('/IMAGE FILE/Working Tomato 1.svg')){
-        $tomato.attr('src','/IMAGE FILE/Working Tomato 2.svg');
+    if ($tomato.attr('src') == ('andygiovanny.github.io/IMAGE FILE/Working Tomato 1.svg')){
+        $tomato.attr('src','andygiovanny.github.io/IMAGE FILE/Working Tomato 2.svg');
     }
     
-    else if($tomato.attr('src') == ('/IMAGE FILE/Chill Tomato.svg')){
-        $tomato.attr('src','/IMAGE FILE/Chill Tomato 2.svg');
+    else if($tomato.attr('src') == ('andygiovanny.github.io/IMAGE FILE/Chill Tomato.svg')){
+        $tomato.attr('src','andygiovanny.github.io/IMAGE FILE/Chill Tomato 2.svg');
     }
      
-    else if($tomato.attr('src') == ('/IMAGE FILE/Chill Tomato 2.svg')){
-        $tomato.attr('src','/IMAGE FILE/Chill Tomato.svg');
+    else if($tomato.attr('src') == ('andygiovanny.github.io/IMAGE FILE/Chill Tomato 2.svg')){
+        $tomato.attr('src','andygiovanny.github.io/IMAGE FILE/Chill Tomato.svg');
     }
     
-    else if($tomato.attr('src') == ('/IMAGE FILE/tea tomato.svg')){
-        $tomato.attr('src','/IMAGE FILE/tea tomato 2.svg');
+    else if($tomato.attr('src') == ('andygiovanny.github.io/IMAGE FILE/tea tomato.svg')){
+        $tomato.attr('src','andygiovanny.github.io/IMAGE FILE/tea tomato 2.svg');
     }
     
-    else if($tomato.attr('src') == ('/IMAGE FILE/tea tomato 2.svg')){
-        $tomato.attr('src','/IMAGE FILE/tea tomato.svg');
+    else if($tomato.attr('src') == ('andygiovanny.github.io/IMAGE FILE/tea tomato 2.svg')){
+        $tomato.attr('src','andygiovanny.github.io/IMAGE FILE/tea tomato.svg');
     }
     
     else{
-        $tomato.attr('src','/IMAGE FILE/Working Tomato 1.svg'); 
+        $tomato.attr('src','andygiovanny.github.io/IMAGE FILE/Working Tomato 1.svg'); 
     }  
   }
 
@@ -51,17 +51,17 @@ $('.navigation').on('click', function(){
     
 
 $('#shortButton').on('click', function(){
-    $tomato.attr('src','/IMAGE FILE/Chill Tomato 2.svg');
+    $tomato.attr('src','andygiovanny.github.io/IMAGE FILE/Chill Tomato.svg');
     clearInterval();
   });
 
 $('#pomodoroButton').on('click', function(){
-    $tomato.attr('src','/IMAGE FILE/Working Tomato 1.svg');
+    $tomato.attr('src','andygiovanny.github.io/IMAGE FILE/Working Tomato 1.svg');
     clearInterval();
   });
 
 $('#longButton').on('click', function(){
-    $tomato.attr('src','/IMAGE FILE/tea tomato.svg');
+    $tomato.attr('src','andygiovanny.github.io/IMAGE FILE/tea tomato.svg');
     clearInterval();
   });
 
